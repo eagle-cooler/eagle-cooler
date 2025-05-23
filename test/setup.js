a473d05bf02b10ad39ea5b509f67f1e30b094cd4
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2025-05-22 18:44:12 -0700

    fix: allow correct build

---
+++ test/setup.js
@@ -0,0 +1,5 @@
+"use strict";
+// This file is used to set up the test environment
+// Add any global test setup here
+// Example: Set up any global variables or mocks
+global.console = Object.assign({}, console);
